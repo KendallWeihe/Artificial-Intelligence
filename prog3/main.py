0b38c444551b826c3801417a83feee6e2a14bb8e
Kendall Weihe <kendallweihe@gmail.com>
2016-09-26 11:50:42 -0400

    I think I have dpll fully functional

--- prog3/main.py
+++ prog3/main.py
@@ -20,13 +20,20 @@ sys.setrecursionlimit(100000) # 10000 is an example, try with different values
 def consistent_set_of_literals(phi):
     explored_literals = []
     # pdb.set_trace()
+    all_empty = True
     for i in range(phi[:,0].shape[0]):
         for j in range(phi[0,:].shape[0]):
             if phi[i,j] not in explored_literals:
                 explored_literals.append(phi[i,j])
-                if explored_literals[len(explored_literals)-1] != 0 and -explored_literals[len(explored_literals)-1] in phi:
-                    return False
-    return True
+                if explored_literals[len(explored_literals)-1] != 0:
+                    all_empty = False
+                    if -explored_literals[len(explored_literals)-1] in phi:
+                        return False
+
+    if all_empty:
+        return False
+    else:
+        return True
 
 def contains_empty_clause(phi):
     for i in range(phi[:,0].shape[0]):
@@ -54,8 +61,7 @@ def find_pure_literals(phi):
         for j in range(phi[0,:].shape[0]):
             if phi[i,j] not in explored_literals:
                 explored_literals.append(phi[i,j])
-            else:
-                if -explored_literals[len(explored_literals)-1] not in phi:
+                if explored_literals[len(explored_literals)-1] != 0 and -explored_literals[len(explored_literals)-1] not in phi:
                     pure_literals.append(phi[i,j])
     return np.array(pure_literals)
 
@@ -65,18 +71,18 @@ def dpll(phi):
     # if phi.shape[0] == 1:
     #     pdb.set_trace()
     #     return
-
+    temp = phi.copy()
     if consistent_set_of_literals(phi):
-        # pdb.set_trace()
         return True
     if contains_empty_clause(phi):
-        # pdb.set_trace()
         return False
     unit_clauses = find_unit_clauses(phi)
     for unit_clause in unit_clauses:
         # set unit_clause to true
         delete_these_indices = np.where(phi==unit_clause)[0]
         phi = np.delete(phi, (delete_these_indices), 0)
+        # pdb.set_trace()
+        np.place(phi,phi == -unit_clause, 0)
         if phi.shape[0] == 0:
             # pdb.set_trace()
             phi = np.array([[0,0,0,0]])
@@ -88,29 +94,27 @@ def dpll(phi):
         phi = np.delete(phi, (delete_these_indices), 0)
         if phi.shape[0] == 0:
             # pdb.set_trace()
+            return True
             phi = np.array([[0,0,0,0]])
 
 
-    if contains_empty_clause(phi):
-        # pdb.set_trace()
-        return False
-
-    # if phi.shape[0] == 0:
-    #     pdb.set_trace()
-    #     return False
-
     first_literal = None
+    found = False
     for i in range(phi[:,0].shape[0]):
         temp_clause = phi[i,:].copy()
         temp_clause = temp_clause[temp_clause != 0]
-        # for j in range(temp_clause.shape[0]):
-            # if temp_clause[j] not in explored_vars:
-                # first_literal = temp_clause[j]
-        first_literal = temp_clause[0]
+        for j in range(temp_clause.shape[0]):
+            if temp_clause[j] != 0:
+                first_literal = temp_clause[j]
+                found = True
+                break
+        if found == True:
+            break
 
-    # if first_literal == None:
-    #     pdb.set_trace()
-    #     return False
+    if first_literal == None:
+        # pdb.set_trace()
+        print
+        first_literal = 0
 
     additional_tuple = np.zeros((phi[0,:].shape[0]))
     additional_tuple[0] = first_literal
@@ -133,6 +137,8 @@ def main():
             print "Formula -- " + filename + "--------------------"
             formula = np.genfromtxt(difficulty + filename)
             formula = formula[1:formula[:,0].shape[0],:].copy()
+            pdb.set_trace()
+
             for i in range(num_algs):
                 if i == 0:
                     print "     Running the DPLL algorithm..."
