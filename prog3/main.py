78442de4123bb6913559b6615d1b84e45fcd845d
Kendall Weihe <kendallweihe@gmail.com>
2016-09-26 16:13:29 -0400

    blank commit

--- prog3/main.py
+++ prog3/main.py
@@ -2,20 +2,9 @@ import numpy as np
 import os
 import pdb
 import sys
-sys.setrecursionlimit(100000) # 10000 is an example, try with different values
+# sys.setrecursionlimit(100000) # 10000 is an example, try with different values
 
-#TODO
-    # DPLL alg:
-    #     call function to check if phi is a consistent set of literals (all pure literals)
-    #     call function to check if phi contains an empty set
-    #     for every unit clause l
-    #         set l to true
-    #         remove all clauses that contain l
-    #     for every pure literal
-    #         set l to true
-    #         remove l from all clauses
-    #     l' <-- select first literal from phi
-    #     return dpll(phi & l) || dpll(phi & !l)
+# DPLL --------------------------------------------------------------------------
 
 def consistent_set_of_literals(phi):
     explored_literals = []
@@ -65,36 +54,25 @@ def find_pure_literals(phi):
                     pure_literals.append(phi[i,j])
     return np.array(pure_literals)
 
-
 explored_vars = []
 def dpll(phi):
-    # if phi.shape[0] == 1:
-    #     pdb.set_trace()
-    #     return
-    temp = phi.copy()
     if consistent_set_of_literals(phi):
         return True
     if contains_empty_clause(phi):
         return False
     unit_clauses = find_unit_clauses(phi)
     for unit_clause in unit_clauses:
-        # set unit_clause to true
         delete_these_indices = np.where(phi==unit_clause)[0]
         phi = np.delete(phi, (delete_these_indices), 0)
-        # pdb.set_trace()
         np.place(phi,phi == -unit_clause, 0)
         if phi.shape[0] == 0:
-            # pdb.set_trace()
             phi = np.array([[0,0,0,0]])
 
     pure_literals = find_pure_literals(phi)
     for pure_literal in pure_literals:
-        # np.place(phi, phi==pure_literal, 1)
         delete_these_indices = np.where(phi==pure_literal)[0]
         phi = np.delete(phi, (delete_these_indices), 0)
         if phi.shape[0] == 0:
-            # pdb.set_trace()
-            return True
             phi = np.array([[0,0,0,0]])
 
 
@@ -112,8 +90,6 @@ def dpll(phi):
             break
 
     if first_literal == None:
-        # pdb.set_trace()
-        print
         first_literal = 0
 
     additional_tuple = np.zeros((phi[0,:].shape[0]))
@@ -125,9 +101,15 @@ def dpll(phi):
 
     return dpll(phi_and_first_literal) or dpll(phi_and_first_literal_not)
 
+# -------------------------------------------------------------------------------
+
+# LOCAL SEARCH ------------------------------------------------------------------
+
+
+
+# -------------------------------------------------------------------------------
+
 def main():
-    # iterate over easy and hard
-    #     read in filenames
     num_algs = 3
     num_trials = 10
     difficulties = ["easy/", "hard/"]
@@ -137,7 +119,6 @@ def main():
             print "Formula -- " + filename + "--------------------"
             formula = np.genfromtxt(difficulty + filename)
             formula = formula[1:formula[:,0].shape[0],:].copy()
-            pdb.set_trace()
 
             for i in range(num_algs):
                 if i == 0:
@@ -148,8 +129,7 @@ def main():
                     print "     Running the WalkSAT algorithm..."
 
                 for j in range(num_trials):
-                    print "Trial #" + str(j)
-                    # pdb.set_trace()
+                    print "         Trial #" + str(j)
                     if i == 0:
                         if filename == "3.cnf":
                             pdb.set_trace()
@@ -160,11 +140,11 @@ def main():
                     #     satisfiable, c = walk_sat(formula)
 
                     if satisfiable == True:
-                        print "This formula is satisfiable"
-                        print "Time taken was: "
+                        print "     This formula is satisfiable"
+                        print "     Time taken was: "
                     else:
-                        print "The algorithm was unable to find a satisfiable solution"
-                        print "Time taken was: "
+                        print "     The algorithm was unable to find a satisfiable solution"
+                        print "     Time taken was: "
             print "-------------------------------------------------"
 
 main()
