e973f75ab3a01153cc9c90f583d78f0c5b382080
Kendall Weihe <kendallweihe@gmail.com>
2016-09-27 14:31:21 -0400

    empty commit

--- prog3/main.py
+++ prog3/main.py
@@ -105,7 +105,97 @@ def dpll(phi):
 
 # LOCAL SEARCH ------------------------------------------------------------------
 
+def generate_neighbors(current_state):
 
+    # for i in range(possible_neighbors[:,0].shape[0]):
+    #     mask = np.bitwise_and(current_state, possible_neighbors[i,:])
+    #     one_different = 0
+    #     for j in range(mask.shape[0]):
+    #         if current_state[j] != mask[j]:
+    #             one_different = one_different + 1
+    #         if one_different > 1:
+    #             break
+    #     if one_different == 1:
+    #         neighbor.append(possible_neighbors[i,:])
+
+    neighbors = []
+    for i in range(current_state.shape[0]):
+        neighbor = np.zeros((current_state.shape[0]))
+        for j in range(current_state.shape[0]):
+            if j == i:
+                neighbor[j] = int(not(current_state[j]))
+            else:
+                neighbor[j] = current_state[j]
+        neighbors.append(neighbor)
+
+    return np.array(neighbors)
+
+def eval_current_state(current_state, formula):
+    clause_count = 0
+    for i in range(formula[:,0].shape[0]):
+        # x(i) == index-1
+        variable_correct_count = 0
+        for j in range(formula[0,:].shape[0]):
+            if formula[i,j] > 0:
+                if current_state[formula[i,j]-1] == 1:
+                    variable_correct_count = variable_correct_count + 1
+            else:
+                if current_state[(-1 * formula[i,j])-1] == 0:
+                    variable_correct_count = variable_correct_count + 1
+        if variable_correct_count > 0:
+            clause_count = clause_count + 1
+
+    return clause_count
+
+def eval_neighbors(current_state, formula):
+
+    neighbors = generate_neighbors(current_state)
+
+    num_clauses_satisfied = []
+    for i in range(neighbors[:,0].shape[0]):
+        clause_count = 0
+        for j in range(formula[:,0].shape[0]):
+            # x(i) == index-1
+            variable_correct_count = 0
+            for k in range(formula[0,:].shape[0]):
+                if formula[j,k] > 0:
+                    if neighbors[i,formula[j,k]-1] == 1:
+                        variable_correct_count = variable_correct_count + 1
+                else:
+                    if neighbors[i,(-1 * formula[j,k])-1] == 0:
+                        variable_correct_count = variable_correct_count + 1
+            if variable_correct_count > 0:
+                clause_count = clause_count + 1
+        num_clauses_satisfied.append(clause_count)
+
+    num_clauses_satisfied = np.array(num_clauses_satisfied)
+    index_of_optimal_neighbor = np.argmax(num_clauses_satisfied,axis=0)
+
+    if eval_current_state(current_state, formula) < num_clauses_satisfied[index_of_optimal_neighbor]:
+        return neighbors[np.argmax(num_clauses_satisfied,axis=0),:]
+    else:
+        return -1
+
+def local_search(formula, starting_state):
+    #TODO
+        # while 1
+        #     optimal_neighbor = eval_neighbors(random_start_state, formula)
+        #     if optimal_neighbor == -1:
+        #         return current state
+        #     else:
+        #         neighbor is better, keep going
+        #         current_state = optimal_neighbor
+
+def hill_climbing(formula):
+    #TODO
+        # set timer
+        # call local_search
+        # if return:
+        #     evaluate function for satisfiability
+            # if satsified: return
+            # else: call local_search again with new starting state
+        # else:
+        #     return -- no satisified solution found
 
 # -------------------------------------------------------------------------------
 
@@ -115,6 +205,7 @@ def main():
     difficulties = ["easy/", "hard/"]
     for difficulty in difficulties:
         filenames = os.listdir(difficulty)
+        pdb.set_trace()
         for filename in filenames:
             print "Formula -- " + filename + "--------------------"
             formula = np.genfromtxt(difficulty + filename)
@@ -131,8 +222,6 @@ def main():
                 for j in range(num_trials):
                     print "         Trial #" + str(j)
                     if i == 0:
-                        if filename == "3.cnf":
-                            pdb.set_trace()
                         satisfiable = dpll(formula)
                     # elif i == 1:
                     #     satisfiable, c = local_search(formula)
