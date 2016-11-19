0afe1651c8aa29fd68304d909200f9502bf9882b
Kendall Weihe <kendallweihe@gmail.com>
2016-11-18 19:30:45 -0500

    graph

--- prolog2/README.txt
+++ prolog2/README.txt
@@ -1,19 +1,114 @@
+Author: Kendall Weihe
+Assignment: CS463 Prolog assignment 2
+Goal: navigate a city maze, collect pokemon, eggs, and pokeballs (order matters)
+Date: November 4th 2016
 
+--------------------------------------------------------------------
 
+How to run:
+
+  - this program MUST be run with gprolog (I use functions unique to gprolog)
+  - I run the program like the following example (sorry so many warnings):
+
+      Kendalls-MacBook-Pro:prolog2 kendallweihe$ gprolog
+      GNU Prolog 1.4.4 (64 bits)
+      Compiled Apr 23 2013, 17:26:17 with /opt/local/bin/gcc-apple-4.2
+      By Daniel Diaz
+      Copyright (C) 1999-2013 Daniel Diaz
+      | ?- consult(prolog2).
+      compiling /Users/kendallweihe/Google Drive/Academic/Fall 2016/CS463/prolog2/prolog2.pl for byte code...
+      /Users/kendallweihe/Google Drive/Academic/Fall 2016/CS463/prolog2/prolog2.pl:3: warning: singleton variables [Path] for global_egg_path/1
+      /Users/kendallweihe/Google Drive/Academic/Fall 2016/CS463/prolog2/prolog2.pl:4: warning: singleton variables [Visited] for global_egg_visited/1
+      /Users/kendallweihe/Google Drive/Academic/Fall 2016/CS463/prolog2/prolog2.pl:5: warning: singleton variables [Path] for global_pika_path/1
+      /Users/kendallweihe/Google Drive/Academic/Fall 2016/CS463/prolog2/prolog2.pl:6: warning: singleton variables [Visited] for global_pika_visited/1
+      /Users/kendallweihe/Google Drive/Academic/Fall 2016/CS463/prolog2/prolog2.pl:7: warning: singleton variables [Path] for global_mb_path/1
+      /Users/kendallweihe/Google Drive/Academic/Fall 2016/CS463/prolog2/prolog2.pl:8: warning: singleton variables [Visited] for global_mb_visited/1
+      /Users/kendallweihe/Google Drive/Academic/Fall 2016/CS463/prolog2/prolog2.pl:9: warning: singleton variables [Path] for global_mt_path/1
+      /Users/kendallweihe/Google Drive/Academic/Fall 2016/CS463/prolog2/prolog2.pl:10: warning: singleton variables [Visited] for global_mt_visited/1
+      /Users/kendallweihe/Google Drive/Academic/Fall 2016/CS463/prolog2/prolog2.pl:13-56: warning: singleton variables [Score,X4,Y4] for maze/5
+      /Users/kendallweihe/Google Drive/Academic/Fall 2016/CS463/prolog2/prolog2.pl compiled, 296 lines read - 40543 bytes written, 17 ms
+
+      (6 ms) yes
+      | ?- maze(1,1,[[ e,  e,  e,  o,  mb], [ o,  j,  o,  o,  o], [ o,  j, mt,  j,  o], [ o,  o,  e, o, o],[ p,  o,  j, o,  p]], Path, 0).
+      11[[3,3],[3,2],[4,2],[5,2],[5,3],[5,4],[4,4],[3,4],[2,4],[1,4],[1,3],[1,2],[1,1],[2,1],[3,1],[4,1],5,1][[5,1],[4,1],[3,1],[2,1],[1,1],[1,2],[1,3],[1,4],[2,4],[3,4],[4,4],[4,5],5,5][[5,5],[5,4],[4,4],[3,4],[2,4],[1,4],[1,3],[1,2],[1,1],[2,1],[3,1],4,1][[4,1],[3,1],[2,1],[1,1]|_463]
+
+      true ?
+
+      (1 ms) yes
+
+--------------------------------------------------------------------
+
+The overall maze predicate is defined as follows:
+find_and_hatch_egg() AND find_pikachu() AND find_mb() AND find_mt() --> maze()
+
+Each "sub-predicate" (all of the "find" predicates from the above predicate) have the following general design:
+[walk_left AND search] OR [walk_right AND search] OR [walk_down AND search] OR [walk_up AND search] --> find*()
+
+find_and_hatch_egg() returns true once the egg has been found and the person has walked three blocks.
+find_pikachu() returns true once pikachu is found.
+find_mb() returns true once the masterball is found.
+find_mt() returns true once mewtwo has been found.
+The above predicates happen in the exact order (the masterball must be found before mewtwo).
+After each predicate, the ending coordinates from the previous predicate serves as the starting coordinates for the next predicate.
+
+--------------------------------------------------------------------
+
+I store two types of global variables: blocks that have been visited and paths for each of the "sub-predicates" above.
+After the maze has completed, I can read the global path and print it to the display.
+Also, storing which blocks have been visited for each "sub-predicate" prevents the algorithm from entering infinite recursion.
+
+--------------------------------------------------------------------
+
+The output looks like this (sorry it isn't pretty):
+  11[[3,3],[3,2],[4,2],[5,2],[5,3],[5,4],[4,4],[3,4],[2,4],[1,4],[1,3],[1,2],[1,1],[2,1],[3,1],[4,1],5,1][[5,1],[4,1],[3,1],[2,1],[1,1],[1,2],[1,3],[1,4],[2,4],[3,4],[4,4],[4,5],5,5][[5,5],[5,4],[4,4],[3,4],[2,4],[1,4],[1,3],[1,2],[1,1],[2,1],[3,1],4,1][[4,1],[3,1],[2,1],[1,1]|_463]
+
+The "11" at the verying beginning is the score.
+The list of list is the path the person took -- in reverse order. There are multiple path lists -- once for each "sub-predicate"
+
+--------------------------------------------------------------------
+
+Throughout the code you will see a lot of code blocks that look like the following:
+
+( Prev_direction \= 1, %walk left
+  LeftX is X-1, %subtract index
+  LeftX > 0, %make sure new index is not out of bounds in the negative direction
+  is_x_within_bounds(LeftX, Y, Maze), %make sure index is not beyond the maximum size of the maze
+  is_open(LeftX, Y, Maze), %check if the block is an open block
+  find_egg_and_walk(LeftX, Y, Maze, [[LeftX, Y] | Path], 2)); %recursive call the continue searching for eggs
+
+The above code block is declaring the person walks left. I have added comments for every line in this
+code block, but all the rest will not contain all comments -- use this for reference.
+
+--------------------------------------------------------------------
 
 Test data:
-
-[[ o,  e,  j,  p,  o], [ o,  j,  o,  o,  o], [ o,  j, mt,  j,  o], [ o,  o,  e, o, o]],[ p,  o,  j, mb,  o]]
-
-[[ 'o',  'e',  'j',  'p',  'o'], [ 'o',  'j',  'o',  'o',  'o'], [ 'o',  'j', 'm't,  'j',  'o'], [ 'o',  'o',  'e', 'o', 'o']],[ 'p',  'o',  'j', 'm'b,  'o']]
-
-
-maze(1,1,[[ 'o',  'e',  'j',  'p',  'o'], [ 'o',  'j',  'o',  'o',  'o'], [ 'o',  'j', 'mt',  'j',  'o'], [ 'o',  'o',  'e', 'o', 'o'],[ 'p',  'o',  'j', 'mb',  'o']], Path, 0).
-
-maze(1,1,[[ e,  e,  j,  p,  o], [ o,  j,  o,  o,  o], [ o,  j, mt,  j,  o], [ o,  o,  e, o, o],[ p,  o,  j, mb,  o]], Path, 0).
-
+//from canvas
 maze(1,1,[[ e,  e,  e,  o,  o], [ o,  j,  o,  o,  p], [ o,  j, mt,  j,  o], [ o,  o,  e, o, o],[ p,  o,  j, mb,  o]], Path, 0).
 
+//placed pikachu much further away
 maze(1,1,[[ e,  e,  e,  o,  o], [ o,  j,  o,  o,  o], [ o,  j, mt,  j,  o], [ o,  o,  e, o, o],[ p,  o,  j, mb,  p]], Path, 0).
 
+//placed masterball further away from pikachu so that the person will have to backtrack
 maze(1,1,[[ e,  e,  e,  o,  mb], [ o,  j,  o,  o,  o], [ o,  j, mt,  j,  o], [ o,  o,  e, o, o],[ p,  o,  j, o,  p]], Path, 0).
+
+//unsolvable
+maze(1,1,[[ e,  j,  j,  o,  mb], [ j,  j,  o,  o,  o], [ o,  j, mt,  j,  o], [ o,  o,  e, o, o],[ p,  o,  j, o,  p]], Path, 0).
+
+--------------------------------------------------------------------
+
+Notes:
+  - this is a very naive implementation -- I think this algorithm could be implemented in much fewer lines of code
+  - a lot of the major predicates have overlapping features (walk_left, walk_right, etc.)
+    - it would be nice if I could somehow simplify this
+
+--------------------------------------------------------------------
+
+What I learned:
+  - prolog does not return values
+    - I thought I could instantiate variables in an invoked predicate such that
+        when I assigned values to them in the invoked predicate, the value would return to the callee's scope
+    - I had to make use of global variables for this reason
+  - declarative programming is much different than functional
+    - I have a difficult time trying to avoid "functional programming" techniques
+  - I learned ways to prevent stack overflows
+    - don't visit blocks that have already been visited
--- prolog2/prolog2.pl
+++ prolog2/prolog2.pl
@@ -1,4 +1,5 @@
 
+%global variables -- paths and blocks already visited
 global_egg_path(Path).
 global_egg_visited(Visited).
 global_pika_path(Path).
@@ -8,25 +9,30 @@ global_mb_visited(Visited).
 global_mt_path(Path).
 global_mt_visited(Visited).
 
+%main predicate
 maze(X, Y, Maze, Path, Score) :-
 
-  (g_assign(global_egg_visited, [[0,0]]),
-  find_egg_and_walk(X, Y, Maze, Path, 0),
-  New_score is Score+1,
-  g_read(global_egg_path, Egg_path),
-  Egg_head = Egg_path,
-  nth1(1, Egg_head, Egg_tuple),
-  nth1(1, Egg_tuple, X1),
-  nth1(2, Egg_tuple, Y1)),
+  %find and hatch eggs:
+  (g_assign(global_egg_visited, [[0,0]]), %initialize global Visited variable
+  find_and_hatch_egg(X, Y, Maze, Path, 0), %find and hatch egg
+  Egg_score is 1, %update score
+  g_read(global_egg_path, Egg_path), %read the global path variable (assigned in a deeper predicate)
+  Egg_head = Egg_path, %assign to local variable
+  nth1(1, Egg_head, Egg_tuple), %extract first tuple
+  nth1(1, Egg_tuple, X1), %this is the starting X coordinate for finding pikachu
+  nth1(2, Egg_tuple, Y1)), %this is the starting Y coordinate for finding pikachu
 
+  %find pikachu
   (g_assign(global_pika_visited, [[0,0]]),
-  find_pikachu(X1, Y1, Maze, [X1,Y1], 0),
+  find_pikachu(X1, Y1, Maze, [X1,Y1], 0), %note that the starting coordinates are where find_and_hatch_egg predicate finished
+  Pikachu_score is 10,
   g_read(global_pika_path, Pikachu_path),
   Pikachu_head = Pikachu_path,
   nth1(1, Pikachu_head, Pikachu_tuple),
   nth1(1, Pikachu_tuple, X2),
   nth1(2, Pikachu_tuple, Y2)),
 
+  %find the masterball
   (g_assign(global_mb_visited, [[0,0]]),
   find_mb(X2, Y2, Maze, [X2, Y2], 0),
   g_read(global_mb_path, Mb_path),
@@ -35,6 +41,7 @@ maze(X, Y, Maze, Path, Score) :-
   nth1(1, Mb_tuple, X3),
   nth1(2, Mb_tuple, Y3)),
 
+  %find mewtwo
   (g_assign(global_mt_visited, [[0,0]]),
   find_mt(X3, Y3, Maze, [X3, Y3], 0),
   g_read(global_mt_path, Mt_path),
@@ -43,56 +50,63 @@ maze(X, Y, Maze, Path, Score) :-
   nth1(1, Mt_tuple, X4),
   nth1(2, Mt_tuple, Y4)),
 
+  %compute and report final score and path to destination
+  Final_score is Egg_score+Pikachu_score,
+  write(Final_score),
   write(Mt_head), write(Mb_head), write(Pikachu_head), write(Egg_head).
 
-find_egg_and_walk(X, Y, Maze, Path, Prev_direction) :-
+%predicate returns true once an egg has been found and the person has walked three blocks
+find_and_hatch_egg(X, Y, Maze, Path, Prev_direction) :-
 
-  g_read(global_egg_visited, Visited),
+  g_read(global_egg_visited, Visited), %check if this block has already been visited
   \+member([X,Y], Visited),
   append([[X, Y]], Visited, New_visited),
   g_assign(global_egg_visited, New_visited),
-  (
+  ( %notice this parenthesis incorporates the remainder of the overall find_and_hatch_egg predicate
 
-  (egg_found(X, Y, Maze),
-  walk_three_steps(X, Y, Maze, [[X, Y] | Path], Prev_direction, 0));
+  (egg_found(X, Y, Maze), %true if an egg has been found
+  walk_three_steps(X, Y, Maze, [[X, Y] | Path], Prev_direction, 0)); %now begin walking three blocks
 
-  ( Prev_direction \= 1,
-    LeftX is X-1,
-    LeftX > 0,
-    is_x_within_bounds(LeftX, Y, Maze),
-    is_open(LeftX, Y, Maze),
-    find_egg_and_walk(LeftX, Y, Maze, [[LeftX, Y] | Path], 2));
+  ( Prev_direction \= 1, %walk left
+    LeftX is X-1, %subtract index
+    LeftX > 0, %make sure new index is not out of bounds in the negative direction
+    is_x_within_bounds(LeftX, Y, Maze), %make sure index is not beyond the maximum size of the maze
+    is_open(LeftX, Y, Maze), %check if the block is an open block
+    find_and_hatch_egg(LeftX, Y, Maze, [[LeftX, Y] | Path], 2)); %recursive call the continue searching for eggs
 
-  ( Prev_direction \= 2,
+  ( Prev_direction \= 2, %walk right
     RightX is X+1,
     is_x_within_bounds(RightX, Y, Maze),
     is_open(RightX, Y, Maze),
-    find_egg_and_walk(RightX, Y, Maze, [[RightX, Y] | Path], 1));
+    find_and_hatch_egg(RightX, Y, Maze, [[RightX, Y] | Path], 1));
 
-  ( Prev_direction \= 3,
+  ( Prev_direction \= 3, %walk down
     DownY is Y-1,
     DownY > 0,
     is_y_within_bounds(DownY, Maze),
     is_open(X, DownY, Maze),
-    find_egg_and_walk(X, DownY, Maze, [[X, DownY] | Path], 4));
+    find_and_hatch_egg(X, DownY, Maze, [[X, DownY] | Path], 4));
 
-  ( Prev_direction \= 4,
+  ( Prev_direction \= 4, %walk up
     UpY is Y+1,
     UpY > 0,
     is_y_within_bounds(UpY, Maze),
     is_open(X, UpY, Maze),
-    find_egg_and_walk(X, UpY, Maze, [[X, UpY] | Path], 3))
+    find_and_hatch_egg(X, UpY, Maze, [[X, UpY] | Path], 3))
 
   ).
 
+%predicate returns true if the current block contains an egg
 egg_found(X, Y, Maze) :-
   nth1(Y, Maze, Row),
   nth1(X, Row, e).
 
+%predicate returns true if the person has walked three blocks -- at this point the person has already found an egg
 walk_three_steps(X, Y, Maze, Path, Prev_direction, Num_steps) :-
-  Num_steps == 3, g_assign(global_egg_path, Path);
+  Num_steps == 3,
+  g_assign(global_egg_path, Path); %assign the path to the egg to the global variable
 
-  ( Prev_direction \= 1,
+  ( Prev_direction \= 1, %walk left
     LeftX is X-1,
     LeftX > 0,
     is_x_within_bounds(LeftX, Y, Maze),
@@ -100,14 +114,14 @@ walk_three_steps(X, Y, Maze, Path, Prev_direction, Num_steps) :-
     Num_steps_new is Num_steps+1,
     walk_three_steps(LeftX, Y, Maze, [[LeftX, Y] | Path], 2, Num_steps_new));
 
-  ( Prev_direction \= 2,
+  ( Prev_direction \= 2, %walk right
     RightX is X+1,
     is_x_within_bounds(RightX, Y, Maze),
     is_open(RightX, Y, Maze),
     Num_steps_new is Num_steps+1,
     walk_three_steps(RightX, Y, Maze, [[RightX, Y] | Path], 1, Num_steps_new));
 
-  ( Prev_direction \= 3,
+  ( Prev_direction \= 3, %walk down
     DownY is Y-1,
     DownY > 0,
     is_y_within_bounds(DownY, Maze),
@@ -115,7 +129,7 @@ walk_three_steps(X, Y, Maze, Path, Prev_direction, Num_steps) :-
     Num_steps_new is Num_steps+1,
     walk_three_steps(X, DownY, Maze, [[X, DownY] | Path], 4, Num_steps_new));
 
-  ( Prev_direction \= 4,
+  ( Prev_direction \= 4, %walk up
     UpY is Y+1,
     UpY > 0,
     is_y_within_bounds(UpY, Maze),
@@ -123,38 +137,41 @@ walk_three_steps(X, Y, Maze, Path, Prev_direction, Num_steps) :-
     Num_steps_new is Num_steps+1,
     walk_three_steps(X, UpY, Maze, [[X, UpY] | Path], 3, Num_steps_new)).
 
+%predicate returns true if a pikachu is found
 find_pikachu(X, Y, Maze, Path, Prev_direction) :-
 
+  %check if the current block has already been visited -- if not, then append the block to the global Visited variable
   g_read(global_pika_visited, Visited),
   \+member([X,Y], Visited),
-  append([[X, Y]], Visited, New_visited),
-  g_assign(global_pika_visited, New_visited),
+  append([[X, Y]], Visited, New_visited), %append the current block to global variable
+  g_assign(global_pika_visited, New_visited), %update the global variable
 
-  (
-  pikachu_found(X, Y, Maze),
-  g_assign(global_pika_path, Path);
+  ( %note this parenthesis encapsulates the remainder of the overall find_pikachu predicate
+  pikachu_found(X, Y, Maze), %check if pikachu has been found
+  g_assign(global_pika_path, Path); %if so, then assign the current path to the global variable
 
-  ( Prev_direction \= 1,
+  %begin walking
+  ( Prev_direction \= 1, %walk left
     LeftX is X-1,
     LeftX > 0,
     is_x_within_bounds(LeftX, Y, Maze),
     is_open(LeftX, Y, Maze),
     find_pikachu(LeftX, Y, Maze, [[LeftX, Y] | Path], 2));
 
-  ( Prev_direction \= 2,
+  ( Prev_direction \= 2, %walk right
     RightX is X+1,
     is_x_within_bounds(RightX, Y, Maze),
     is_open(RightX, Y, Maze),
     find_pikachu(RightX, Y, Maze, [[RightX, Y] | Path], 1));
 
-  ( Prev_direction \= 3,
+  ( Prev_direction \= 3, %walk down
     DownY is Y-1,
     DownY > 0,
     is_y_within_bounds(DownY, Maze),
     is_open(X, DownY, Maze),
     find_pikachu(X, DownY, Maze, [[X, DownY] | Path], 4));
 
-  ( Prev_direction \= 4,
+  ( Prev_direction \= 4, %walk up
     UpY is Y+1,
     is_y_within_bounds(UpY, Maze),
     is_open(X, UpY, Maze),
@@ -162,42 +179,46 @@ find_pikachu(X, Y, Maze, Path, Prev_direction) :-
 
   ).
 
+%predicate returns true if the current block contains a pikachu
 pikachu_found(X, Y, Maze) :-
   nth1(Y, Maze, Row),
   nth1(X, Row, p).
 
+%predicate returns true if a masterball has been found
 find_mb(X, Y, Maze, Path, Prev_direction) :-
 
+  %check if the current block has already been visited
   g_read(global_mb_visited, Visited),
   \+member([X,Y], Visited),
-  append([[X, Y]], Visited, New_visited),
+  append([[X, Y]], Visited, New_visited), %if not, then append to global variable
   g_assign(global_mb_visited, New_visited),
-  (
 
-  mb_found(X, Y, Maze),
-  g_assign(global_mb_path, Path);
+  ( %note this parenthesis encapsulates the remainder of the overall find_mb predicate
+  mb_found(X, Y, Maze), %check if masterball is found
+  g_assign(global_mb_path, Path); %if so, then assign the path to the global variable
 
-  ( Prev_direction \= 1,
+  %begin walking
+  ( Prev_direction \= 1, %walk left
     LeftX is X-1,
     LeftX > 0,
     is_x_within_bounds(LeftX, Y, Maze),
     is_open(LeftX, Y, Maze),
     find_mb(LeftX, Y, Maze, [[LeftX, Y] | Path], 2));
 
-  ( Prev_direction \= 2,
+  ( Prev_direction \= 2, %walk right
     RightX is X+1,
     is_x_within_bounds(RightX, Y, Maze),
     is_open(RightX, Y, Maze),
     find_mb(RightX, Y, Maze, [[RightX, Y] | Path], 1));
 
-  ( Prev_direction \= 3,
+  ( Prev_direction \= 3, %walk down
     DownY is Y-1,
     DownY > 0,
     is_y_within_bounds(DownY, Maze),
     is_open(X, DownY, Maze),
     find_mb(X, DownY, Maze, [[X, DownY] | Path], 4));
 
-  ( Prev_direction \= 4,
+  ( Prev_direction \= 4, %walk up
     UpY is Y+1,
     is_y_within_bounds(UpY, Maze),
     is_open(X, UpY, Maze),
@@ -205,45 +226,47 @@ find_mb(X, Y, Maze, Path, Prev_direction) :-
 
   ).
 
-
+%predicate returns true if the current block contains a masterball
 mb_found(X, Y, Maze) :-
   nth1(Y, Maze, Row),
   nth1(X, Row, mb).
 
-
+%predicate returns true if a mewtwo is found
 find_mt(X, Y, Maze, Path, Prev_direction) :-
 
+  %check if the current block has already been visited
   g_read(global_mt_visited, Visited),
   \+member([X,Y], Visited),
-  append([[X, Y]], Visited, New_visited),
+  append([[X, Y]], Visited, New_visited), %if so, update the global Visited variable
   g_assign(global_mt_visited, New_visited),
-  (
 
+  ( %note this parenthesis encapsulates the remainder of the find_mt predicate
   %write(X), write(Y),
   mt_found(X, Y, Maze),
   g_assign(global_mt_path, Path);
 
-  ( Prev_direction \= 1,
+  %begin walking
+  ( Prev_direction \= 1, %walk left
     LeftX is X-1,
     LeftX > 0,
     is_x_within_bounds(LeftX, Y, Maze),
     is_open(LeftX, Y, Maze),
     find_mt(LeftX, Y, Maze, [[LeftX, Y] | Path], 2));
 
-  ( Prev_direction \= 2,
+  ( Prev_direction \= 2, %walk right
     RightX is X+1,
     is_x_within_bounds(RightX, Y, Maze),
     is_open(RightX, Y, Maze),
     find_mt(RightX, Y, Maze, [[RightX, Y] | Path], 1));
 
-  ( Prev_direction \= 3,
+  ( Prev_direction \= 3, %walk down
     DownY is Y-1,
     DownY > 0,
     is_y_within_bounds(DownY, Maze),
     is_open(X, DownY, Maze),
     find_mt(X, DownY, Maze, [[X, DownY] | Path], 4));
 
-  ( Prev_direction \= 4,
+  ( Prev_direction \= 4, %walk up
     UpY is Y+1,
     is_y_within_bounds(UpY, Maze),
     is_open(X, UpY, Maze),
@@ -251,20 +274,23 @@ find_mt(X, Y, Maze, Path, Prev_direction) :-
 
   ).
 
-
+%predicate returns true if the current block contains a mewtwo
 mt_found(X, Y, Maze) :-
   nth1(Y, Maze, Row),
   nth1(X, Row, mt).
 
+%predicate returns true if the current X index is within the Maze boundaries
 is_x_within_bounds(X, Y, Maze) :-
   nth1(Y, Maze, Row), %predicate always true -- instantiates Row
   length(Row, Size), %predicate always true -- instantiates Size
   X =< Size. %predicate true if X < Size
 
+%predicate returns true if the current Y index is within the Maze boundaries
 is_y_within_bounds(Y, Maze) :-
   length(Maze, Size), %predicate always true -- instantiates Size
   Y =< Size. %predicate true if Y < Size
 
+%predicate returns true if the current block is open
 is_open(X, Y, Maze) :-
   nth1(Y, Maze, Row),
   \+ nth1(X, Row, j).
