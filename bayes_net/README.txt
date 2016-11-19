6da2588c33ac515105a346ee230136eb2d540dc2
Kendall Weihe <kendallweihe@gmail.com>
2016-11-18 19:26:06 -0500

    bayes

--- bayes_net/README.txt
+++ bayes_net/README.txt
@@ -1,8 +1,13 @@
+Author: Kendall Weihe
+Assignment: CS463G Fall 2016 Bayes Net programming assignment
+Date: November 18th 2016
+Purpose: To teach students about Bayes Nets and a basic MCMC algorithm
 
+The CPT's of A,D,E and F conditioned on their Markov blankets are stored in the following files.
+In order to understand each table, please refer to this key that shows what each column represents.
+I have also included pictures of the hand written tables -- tables1.jpeg tables2.jpeg
 
-Conditional probabilities given Markov blanket tables keys (columns):
-
-  cp_mb_A.csv:
+  cp_mb_A.csv: (1 = True || 0 = False)
     C | D | a | ~a
 
   cp_mb_D.csv:
@@ -13,3 +18,40 @@ Conditional probabilities given Markov blanket tables keys (columns):
 
   cp_mb_F.csv:
     D | E | f | ~f
+
+Calculating the condition probability given the Markov blanket for each node was challenging, but once I understood
+  certain identities I was able to figure it out. Also, Adam Whidden provided some help on the discussion
+  board. I verified some of my table values with several people on the discussion board: Olivia Houghton and Kristina Shaffer.
+
+  Basic identities:
+    P(AB) = P(A|B)*P(B) = P(B|A)*P(A)
+    P(A) = P(A|B)*P(B) + P(A|~B)P(~B)
+      --> given that A depends on B
+    P(A|B) = P(B|A)*P(A) / P(B)
+    P(A|B) = P(AB) / P(B)
+
+To run the program:
+  - navigate to this directory in the terminal
+  - `python main.py`
+
+Program notes:
+  This is a rather naive implementation. This algorithm could be written in fewer lines of code.
+  The reason why I went with a naive solution was so that I could allow the user to
+  change the values of the probability tables stored in CSV files.
+
+  The MCMC algorithm is run 5 different times, with 10,000 iterations for each run.
+
+  The program saves the ratio values for each of the 5 runs to CSV files.
+  Check the file graph.xls for graphs of the runs.
+
+  General pseudocode is:
+    iterate over number of runs
+      initialize random starting states -- fix evidence nodes (C and B)
+      iterate 10,000 times
+        for each node:
+          find current row in CPT conditioned given the Markov blanket
+          flip a biased coin
+          if the flip > current node value then flip the state of the node
+        if a == true then increment count
+        if iteration % 40 == 0 then append count / iteration_number to ratio array
+      save ratio list to CSV file 
--- bayes_net/main.py
+++ bayes_net/main.py
@@ -1,79 +1,82 @@
 import numpy as np
 import pdb
 
-#TODO
-    # open and read files
-    # iterate 5 times
-    #     randomly initialize non evidence nodes
-    #     iterate 10000 times
-    #         iterate over each node
-    #             randomly generate number between 0 and 100
-    #             if value > then flip bit
-    #             if a == true: count
-    #         if step % 40
-    #             append count / step to array
-    #     save ratio array to file
-
+# import table data
 cp_table_A = np.genfromtxt("cp_mb_A.csv", delimiter=",")
 cp_table_D = np.genfromtxt("cp_mb_D.csv", delimiter=",")
 cp_table_E = np.genfromtxt("cp_mb_E.csv", delimiter=",")
 cp_table_F = np.genfromtxt("cp_mb_F.csv", delimiter=",")
 
+# iterate over 5 runs
 for i in range(5):
+
+    # initialize random starting states
     nodes = [np.random.randint(2), 0, 1]
     for j in range(3):
         nodes.append(np.random.randint(2))
 
-    a_true_count = 0
-    ratio_list = []
+    a_true_count = 0 # counter for number of times a is true
+    ratio_list = [] # list to hold ratios
+
+    # iterate over 10000 iterations
     for j in range(1,10000,1):
 
-        random_num = float(np.random.randint(100)) / 100
-        for k in range(cp_table_A.shape[0]):
-            if cp_table_A[k,0] == nodes[2] and cp_table_A[k,1] == nodes[3]:
-                if nodes[0] == 0:
+        # NODE A
+        random_num = float(np.random.randint(100)) / 100 # generate random number
+        for k in range(cp_table_A.shape[0]): # iterate over table rows
+            if cp_table_A[k,0] == nodes[2] and cp_table_A[k,1] == nodes[3]: # case where the current row reflects the state of the net
+                if nodes[0] == 0: # case where A is currently false
                     if random_num > cp_table_A[k,3]:
-                        nodes[0] = 1 - nodes[0]
-                else:
+                        nodes[0] = 1 - nodes[0] # flip the state
+                else: # case where A is currently true
                     if random_num > cp_table_A[k,2]:
                         nodes[0] = 1 - nodes[0]
 
+        # NODE D
         random_num = float(np.random.randint(100)) / 100
         for k in range(cp_table_D.shape[0]):
-            if cp_table_D[k,0] == nodes[0] and cp_table_D[k,1] == nodes[4] and cp_table_D[k,2] == nodes[5]:
-                if nodes[3] == 0:
+            if cp_table_D[k,0] == nodes[0] and cp_table_D[k,1] == nodes[4] and cp_table_D[k,2] == nodes[5]: # case where the current row reflects the state of the net
+                if nodes[3] == 0: # case where D is currently false
                     if random_num > cp_table_D[k,4]:
-                        nodes[3] = 1 - nodes[3]
-                else:
+                        nodes[3] = 1 - nodes[3] # flip the state
+                else: # case where D is currently true
                     if random_num > cp_table_D[k,3]:
-                        nodes[3] = 1 - nodes[3]
+                        nodes[3] = 1 - nodes[3] # flip the state
 
+        # NODE E
         random_num = float(np.random.randint(100)) / 100
         for k in range(cp_table_E.shape[0]):
-            if cp_table_E[k,0] == nodes[1] and cp_table_E[k,1] == nodes[3] and cp_table_E[k,2] == nodes[5]:
-                if nodes[4] == 0:
+            if cp_table_E[k,0] == nodes[1] and cp_table_E[k,1] == nodes[3] and cp_table_E[k,2] == nodes[5]: # case where the current row reflects the state of the net
+                if nodes[4] == 0: # case where E is currently false
                     if random_num > cp_table_E[k,4]:
-                        nodes[4] = 1 - nodes[4]
-                else:
+                        nodes[4] = 1 - nodes[4] # flip the state
+                else: # case where E is currently true
                     if random_num > cp_table_E[k,3]:
-                        nodes[4] = 1 - nodes[4]
+                        nodes[4] = 1 - nodes[4] # flip the state
 
+        # NODE F
         random_num = float(np.random.randint(100)) / 100
         for k in range(cp_table_F.shape[0]):
-            if cp_table_F[k,0] == nodes[3] and cp_table_F[k,1] == nodes[4]:
-                if nodes[5] == 0:
+            if cp_table_F[k,0] == nodes[3] and cp_table_F[k,1] == nodes[4]: # case where the current row reflects the state of the net
+                if nodes[5] == 0: # case where F is currently false
                     if random_num > cp_table_F[k,3]:
-                        nodes[5] = 1 - nodes[5]
-                else:
+                        nodes[5] = 1 - nodes[5] # flip the state
+                else: # case where F is currently true
                     if random_num > cp_table_F[k,2]:
-                        nodes[5] = 1 - nodes[5]
+                        nodes[5] = 1 - nodes[5] # flip the state
 
+        # check if A is currently true
         if nodes[0] == 1:
             a_true_count = a_true_count + 1
 
+        # append the ratio every 40 iterations
         if j % 40 == 0:
             ratio_list.append(float(a_true_count)/float(j))
 
 
+    # save the ratio list to a CSV file
     filename = "/home/kendall/Documents/Development/CS463G/bayes_net/run_" + str(i) + ".csv"
     np.savetxt(filename, np.array(ratio_list), delimiter="\n")
+
+    # print results
+    print "P(A|B = ~b AND C = c) = " + str(np.mean(np.array(ratio_list)))
